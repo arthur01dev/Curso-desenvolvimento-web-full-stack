949706210e7ff0cc1dcaef899fa8cc51c574bbfa
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-07-06 18:46:14 -0300

    Adicionando aula8 na pasta node

---
+++ node/aula8/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send(`
+        <form action="/" method="POST">
+        Nome do cliente: <input type="text" name="nome"><br>
+        Sobreome do cliente: <input type="text" name="sobrenome">
+        <button>Enviar Formulário</button>
+        </form>
+        `);
+});
+
+app.get('/testes/:idUsuarios?/:parametro?', (requisicao, resposta) => {
+    // /profiles/3
+    // /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+    //
+    console.log(requisicao.params);
+    console.log(requisicao.query);
+    resposta.send(requisicao.query.Nome);
+})
+
+app.post('/', (requisicao, resposta) => {
+    console.log(requisicao.body);
+    resposta.send(`Oque você me enviou foi: ${requisicao.body.sobrenome}`);
+})
+
+app.listen(3300, () => {
+    console.log('Acessar http://localhost:3300');
+    console.log('Servidor executando na porta 3300');
+});
